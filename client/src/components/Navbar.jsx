2fb266348c6d4b34ca310cf5f180de916395a51f
devanshdixit01 <devanshdixit01@gmail.com>
2022-04-13 16:59:57 +0000

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,7 +23,9 @@ const Navbar = () => {
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+                <Link to="/">
                 <img src={logo} alt="logo" className='w-32 cursor-pointer' />
+                </Link>
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
                 {["Create", "Profile"].map((item, index) => (
--- client/src/components/ProfileSection/profileSection.jsx
+++ client/src/components/ProfileSection/profileSection.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React, { useContext } from 'react'
 import { WalletContext } from '../../context/WalletContext';
 import { shortenAddress } from '../../utils/shortnerAddress';
@@ -6,9 +7,31 @@ export default function ProfileSection() {
   const { connectWallet, currentAccount} = useContext(WalletContext);
   console.log(currentAccount);
   return (
-    <div>
-      {shortenAddress(currentAccount)}
-      
+    <div className='gradient-bg-welcome'>
+      <div className={classNames(
+        "py-[100px] px-[20%]",
+      )}>
+        <div className={classNames(
+          "text-3xl font-bold sm:text-4xl text-white"
+        )}> 
+          User Profile
+        </div>
+        <div className={classNames("text-1xl")} style={{
+              marginTop: "16px",
+              color: "rgb(140, 140, 140)"
+        }}>
+        You can set preferred display name, create your branded profile URL and manage other personal settings  
+        </div>
+        <div className={classNames(
+            "mt-[24px] "
+        )}>
+          <img className={classNames(
+          "rounded-[50%]"
+        )} src="/images/bg.svg" alt="" />
+        </div>
+        <div className={classNames("mt-[10px] ml-0",
+        'text-2xl text-white')}>{shortenAddress(currentAccount)}</div>
+      </div>
     </div>
   )
 }
