174889133d9d61cf6213e16697e1eb1d4044d007
devanshdixit01 <devanshdixit01@gmail.com>
2022-04-24 10:56:59 +0000

    updated

--- client/src/components/ProfileSection/profileSection.jsx
+++ client/src/components/ProfileSection/profileSection.jsx
@@ -1,36 +1,67 @@
 import classNames from 'classnames';
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { WalletContext } from '../../context/WalletContext';
 import { shortenAddress } from '../../utils/shortnerAddress';
 
 export default function ProfileSection() {
-  const { connectWallet, currentAccount} = useContext(WalletContext);
-  console.log(currentAccount);
+  const { connectWallet, currentAccount } = useContext(WalletContext);
+  const [onSale, setOnSale] = useState(true);
+  const [owned, setOwned] = useState(false);
+  const [collection, setCollection] = useState(false);
   return (
     <div className='gradient-bg-welcome'>
       <div className={classNames(
-        "py-[100px] px-[20%]",
+        "pt-[100px] pb-[200px] px-[20%]",
       )}>
-        <div className={classNames(
-          "text-3xl font-bold sm:text-4xl text-white"
-        )}> 
-          User Profile
-        </div>
-        <div className={classNames("text-1xl")} style={{
-              marginTop: "16px",
-              color: "rgb(140, 140, 140)"
-        }}>
-        You can set preferred display name, create your branded profile URL and manage other personal settings  
+        <div className='max-w-[1500px] mx-auto rounded-[20px] w-[100%] h-[260px] bg-[#191919]'>
+          <div className={classNames(
+            "pt-[180px] flex justify-center align-bottom"
+          )}>
+            <img className={classNames(
+              "rounded-[50%]  border-2 border-black"
+            )} src="/images/bg.svg" alt="" />
+          </div>
         </div>
         <div className={classNames(
-            "mt-[24px] "
-        )}>
-          <img className={classNames(
-          "rounded-[50%]"
-        )} src="/images/bg.svg" alt="" />
+            'text-gray-400 text-[14px] font-bold mt-[2.5rem] py-1',
+            'bg-[#191919] mx-auto rounded-[20px] px-4 max-w-[370px]'
+          )}>
+            {currentAccount}
+          </div>
+        <div className='mt-[30px]'>
+          <div className={classNames(
+            'flex justify-center'
+          )}>
+            <div className={classNames(
+              'hover:cursor-pointer',
+              'text-[16px] mx-3 font-semibold',
+              onSale ? 'text-white': 'text-gray-500'
+            )} onClick={()=>{
+              setOnSale(!onSale);
+              setCollection(false);
+              setOwned(false);
+            }} >On sale</div>
+            <div className={classNames(
+              'hover:cursor-pointer',
+              'text-[16px] mx-3 font-semibold',
+              owned ? 'text-white': 'text-gray-500'
+            )} onClick={()=>{
+              setOwned(!owned);
+              setCollection(false);
+              setOnSale(false);
+            }}>Owned</div>
+            <div className={classNames(
+              'hover:cursor-pointer',
+              'text-[16px] mx-3 font-semibold',
+              collection ? 'text-white': 'text-gray-500'
+            )} onClick={()=>{
+              setCollection(!collection);
+              setOnSale(false);
+              setOwned(false);
+            }}>Collection</div>
+          </div>
+        <div className=' w-full h-[0.25px] bg-gray-600 mt-5' />
         </div>
-        <div className={classNames("mt-[10px] ml-0",
-        'text-2xl text-white')}>{shortenAddress(currentAccount)}</div>
       </div>
     </div>
   )
