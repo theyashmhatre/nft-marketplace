cd11248ede982237f58e35b250256f9ca2d5e3ba
devanshdixit01 <devanshdixit01@gmail.com>
2022-04-25 21:32:41 +0000

    updated

--- client/src/components/Create/Create.jsx
+++ client/src/components/Create/Create.jsx
@@ -1,10 +1,14 @@
+import classNames from 'classnames'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Create() {
     return (
-        <div className='flex'>
-            <div className='w-1/2 h-100 text-center'><Link to="/create/nft">NFT</Link></div>
+        <div className='flex py-[100px] min-h-[700px]'>
+            <div className={classNames(
+                'w-1/2 my-[10%] h-100 text-center',
+                ''
+                )}><Link to="/create/nft">NFT</Link></div>
             <div className='w-1/2 h-100 text-center'><Link to="/create/collection">Collection</Link></div>
         </div>
     )
