ac71894d428a544f44b91217347cd0049eaf1ebc
devanshdixit01 <devanshdixit01@gmail.com>
2022-04-25 22:05:00 +0000

    updated

--- client/src/components/Create/Create.jsx
+++ client/src/components/Create/Create.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 
 export default function Create() {
     return (
-        <div className='flex py-[100px] min-h-[700px]'>
-            <div className={classNames(
+        <div className='flex justify-around py-[100px] px-[20%] min-h-[700px] bg-[#121212]'>
+            <Link to="/create/nft"><button type="button"  className="lomMVY">NFT</button></Link>
+            <Link to="/create/nft"><button type="button"  className="lomMVY">Collection</button></Link>
+            {/* <div className={classNames(
                 'w-1/2 my-[10%] h-100 text-center',
                 ''
-                )}><Link to="/create/nft">NFT</Link></div>
-            <div className='w-1/2 h-100 text-center'><Link to="/create/collection">Collection</Link></div>
+            )}></div>
+            <div className='w-1/2 h-100 text-center'><Link to="/create/collection">Collection</Link></div> */}
         </div>
     )
 }
